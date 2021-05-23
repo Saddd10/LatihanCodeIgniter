eb42a17dad26754e7ba2e4ad610de77a93f7b540
Saddd10 <73766214+Saddd10@users.noreply.github.com>
2021-05-24 01:44:58 +0700

    Add files via upload

---
+++ application/views/about.php
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+    <link rel="stylesheet" href="<?= base_url('/css/style.css') ?>" type="text/css">
+</head>
+<div class="about-section">
+  <h1>About</h1>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img class="gambar" src="<?= base_url('/css/Foto.jpg') ?>" alt="Arsyad" style="width:50%" >
+      <div class="container">
+        <h2>Muhammad Arsyad Ardiansyah</h2>
+        <p class="title">Institute Technology Sepuluh Nopember Surabaya</p>
+        <p>Eson Wong Gresik</p>
+        <p>
+          <a href="<?= site_url('welcome/contact') ?>">
+          <button class="button">
+            Contact
+          </button>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ application/views/contact.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link rel="stylesheet" href="<?= base_url('/css/style.css') ?>" type="text/css">
+</head>
+<div class="about-section">
+  <h1>Contact</h1>
+</div>
+
+<div class="row">
+  <div class="column2">
+    <div class="card">
+      <div class="container">
+      <p class="title">Name : Muhammad Arsyad Ardiansyah</p>
+        <p class="title">Email : arsyadardiansyah1@gmail.com</p>
+        <p class="title">Telephone : 087854552901</p>
+      </div>
+    </div>
+  </div>
+</div>
